83a0f4758d52d8c7d152ba1fe339d822040a0bb2
agustinsala1996 <agustinsala1996@gmail.com>
2025-01-08 18:31:16 -0300

    prelimnar slider disenio civic h

--- script.js
+++ script.js
@@ -29,3 +29,32 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+// Slider functionality reusable
+let slideIndexx = 1;
+showSlidesx(slideIndexx);
+function plusSlidesx(n) {
+  showSlidesx((slideIndexx += n));
+}
+function currentSlidex(n) {
+  showSlidesx((slideIndexx = n));
+}
+function showSlidesx(n) {
+  let i;
+  let slidesx = document.getElementsByClassName("mySlidesx");
+  let dotsx = document.getElementsByClassName("dotx");
+  if (n > slidesx.length) {
+    slideIndexx = 1;
+  }
+  if (n < 1) {
+    slideIndexx = slidesx.length;
+  }
+  for (i = 0; i < slidesx.length; i++) {
+    slidesx[i].style.display = "none";
+  }
+  for (i = 0; i < dotsx.length; i++) {
+    dotsx[i].className = dotsx[i].className.replace(" activex", "");
+  }
+  slidesx[slideIndexx - 1].style.display = "block";
+  dotsx[slideIndexx - 1].className += " activex";
+}
